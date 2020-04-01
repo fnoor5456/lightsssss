9443dab5fbd48ab5115626c8929479090b8f3651
Noorddin Noorani <noorddinnoorani@Noorddins-iMac.local>
2020-04-01 18:25:58 -0500

    microwave sensor high activates light

--- beaconlight.py
+++ beaconlight.py
@@ -4,9 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(23,GPIO.OUT)
+GPIO.setup(4,GPIO.IN)
 
-print "LED on"
-GPIO.output(23,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(23,GPIO.LOW)
+if GPIO.input(4):
+	print "HIGH SIGNAL RECIEVED"
+	print "LED on"
+	GPIO.output(23,GPIO.HIGH)
+	time.sleep(5)
+	print "LED off"
+	GPIO.output(23,GPIO.LOW)
